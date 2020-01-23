04eb0b2aa73863dc424808de62074abf954feebc
feelsocold <feelsocoold@gmail.com>
2020-01-23 23:59:39 +0900

    ㅠㅠ

--- jex00/src/main/java/org/zerock/config/RootConfig.java
+++ jex00/src/main/java/org/zerock/config/RootConfig.java
@@ -2,6 +2,7 @@ package org.zerock.config;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -27,4 +28,8 @@ public class RootConfig {
 				
 	}
 
+	public SqlSessionFactory sqlSessionFactory() throws Exception{
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactory;
+	}
+	
 }

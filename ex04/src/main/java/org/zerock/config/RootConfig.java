80c95b39ce4968de93abc43785cdcb7db5c2cfb9
feelsocold <feelsocoold@gmail.com>
2020-02-04 22:51:01 +0900

    ~chapter19.스프링에서 트랜잭션 관리

--- ex04/src/main/java/org/zerock/config/RootConfig.java
+++ ex04/src/main/java/org/zerock/config/RootConfig.java
@@ -1,18 +1,38 @@
 package org.zerock.config;
 
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 //JAVA 설정을 이용하는 경우
+/*
+@Configuration
+@ComponentScan(basePackages = {"org.zerock.service"})
+@ComponentScan(basePackages = "org.zerock.aop")
+@EnableAspectJAutoProxy
 
-//@Configuration
-//@ComponentScan(basePackages = {"org.zerock.service"})
-//@ComponentScan(basePackages = "org.zerock.aop")
-//@EnableAspectJAutoProxy
-//
-//@MapperScan(basePackages = {"org.zerock.mapper"})
-//public class RootConfig {
-//
-//}
+	@MapperScan(basePackages = {"org.zerock.mapper"})
+	public class RootConfig {
+	
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		
+	}
+
+	@Bean
+	public SqlSessionFactory sqlSessionFactory() throw Exception{
+		
+	}
+	
+	@Bean
+	public DataSourceTransactioManager txManager(){
+		return new DataSourceTransactionManager(dataSource());
+	
+*/

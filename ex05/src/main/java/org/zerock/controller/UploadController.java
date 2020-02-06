615d6905b91c56e18cfd340f39d9b77a3898b31d
feelsocold <feelsocoold@gmail.com>
2020-02-06 21:27:58 +0900

    ~chatper24.첨부파일의 다운로드 혹은 원본 보여주기

--- ex05/src/main/java/org/zerock/controller/UploadController.java
+++ ex05/src/main/java/org/zerock/controller/UploadController.java
@@ -1,19 +1,77 @@
 package org.zerock.controller;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URLEncoder;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-
+import org.zerock.domain.AttachFileDTO;
 
 import lombok.extern.log4j.Log4j;
+import net.coobird.thumbnailator.Thumbnailator;
+import net.sf.jmimemagic.Magic;
+import net.sf.jmimemagic.MagicMatch;
 
 @Controller
 @Log4j
 public class UploadController {
+	
+	private String getFolder() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		
+		Date date = new Date();
+		String str = sdf.format(date);
+		
+		return str.replace("-", File.separator);
+	}
+	
+	/*
+	private boolean checkImageType(File file) {
+		try {
+			String contentType = Files.probeContentType(file.toPath());
+			log.info("contentType : " + contentType);
+			
+			return contentType.startsWith("image");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	} */
+	
+    private boolean checkImageType(File file){
+        Magic magic = new Magic();
+        try {
+            MagicMatch match = Magic.getMagicMatch(file, false);
+            String contentType = match.getMimeType();
+
+            log.error("CONTENT TYPE : " + contentType);
+            
+            return match.getMimeType().contains("image");
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return false;
+    }    
+	
 	
 	@GetMapping("/uploadForm")
 	public void uploadForm() {
@@ -46,24 +104,153 @@ public class UploadController {
 		log.info("upload AJAX");
 	}
 	
-	@PostMapping("/uploadAjaxAction")
-	public void uploadAjaxPost(MultipartFile[] uploadFile) {
-	String uploadFolder = "/Users/MACBOHAN/eclipse/temp";
+	@PostMapping(value="/uploadAjaxAction",
+				produces = MediaType.APPLICATION_JSON_UTF8_VALUE )
+	@ResponseBody
+	public ResponseEntity<List<AttachFileDTO>> uploadAjaxPost(MultipartFile[] uploadFile) {
 		log.info("uploadAjaxPost()");
+		
+		List<AttachFileDTO> list = new ArrayList<>();
+		String uploadFolder = "/Users/MACBOHAN/eclipse/temp";
+		
+		String uploadFolderPath = getFolder();
+		// make folder -----
+		File uploadPath = new File(uploadFolder, getFolder());
+		log.info("UPLOAD PATH : " + uploadPath);
+		
+		if(uploadPath.exists() == false) {
+			log.info("IT DOESNT EXIST");
+			boolean a = uploadPath.mkdirs();
+			log.info("mkdirs SUCCESS? -- " + a);
+		}
+		// make yyyy/MM/dd folder
 		
 		for(MultipartFile multipartFile : uploadFile) {
 			log.info("-----------------------");
 			log.info("UPLOAD FILE NAME : " + multipartFile.getOriginalFilename());
 			log.info("UPLOAD FILE SIZE : " + multipartFile.getSize());
 			
-			File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
+			AttachFileDTO attachDTO = new AttachFileDTO();
+		
+			String uploadFileName = multipartFile.getOriginalFilename();
+			
+			// IE has file path
+			uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") +  1);
+			log.info("only file name : " + uploadFileName);
+			attachDTO.setFileName(uploadFileName);
+			
+			UUID uuid = UUID.randomUUID();
+			uploadFileName = uuid.toString() + "_" + uploadFileName;
+			log.info("UUID uploadFILENAME : " + uploadFileName);
+			//File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
 			
 			try {
-				multipartFile.transferTo(saveFile);	
+				File saveFile = new File(uploadPath, uploadFileName);
+				multipartFile.transferTo(saveFile);
+				
+				attachDTO.setUuid(uuid.toString());
+				attachDTO.setUploadPath(uploadFolderPath);
+				log.info("!!!!!!!!!!!!"+saveFile);
+				
+				// check image type file
+				if(checkImageType(saveFile)) {
+					
+					attachDTO.setImage(true);
+					
+					FileOutputStream thumbnail 
+						= new FileOutputStream(new File(uploadPath, "s_" + uploadFileName));
+					
+					Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumbnail, 400, 400);
+					
+					thumbnail.close();
+				}
+				
+				//add to List
+				list.add(attachDTO);
+				
+				for (AttachFileDTO a : list) {
+					log.error(a);
+				}
+				
 			} catch (Exception e) {
 				log.error(e.getMessage());
-			}
+			} 
 			
 		} //end for
+		
+		return new ResponseEntity<>(list, HttpStatus.OK);
+	}
+	
+//	섬네일 데이터 전송
+	@GetMapping("/display")
+	@ResponseBody
+	public ResponseEntity<byte[]> getFile(String fileName){
+		
+		log.info("fileName : " + fileName);
+		
+		File file = new File("/Users/MACBOHAN/eclipse/temp/" + fileName);
+		
+		log.info("file : " + file);
+		
+		ResponseEntity<byte[]> result = null;
+		
+		try {
+			HttpHeaders header = new HttpHeaders();
+			
+			MagicMatch match = Magic.getMagicMatch(file, false);
+            String contentType = match.getMimeType();
+            
+			header.add("Content-Type", contentType);
+			result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	@GetMapping(value="/download", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)	// 다운로드를 할 수 있는 MIME 타입
+	@ResponseBody
+	public ResponseEntity<Resource> downloadFile(String fileName
+												, @RequestHeader("User-Agent") String userAgent){
+		log.info("DOWNLOAD File : " + fileName);
+		
+		Resource resource = new FileSystemResource("/Users/MACBOHAN/eclipse/temp//" + fileName);
+		log.info("resource : " + resource);
+
+		if(resource.exists() == false) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+		String resourceName = resource.getFilename();
+		
+		//remove UUID
+		String resourceOriginalName = resourceName.substring(resourceName.indexOf("_") + 1);
+		
+		HttpHeaders headers = new HttpHeaders();
+		
+		try {
+			String downloadName = null;
+			// HTTP 헤더 메시지 중에서 디바이스의 정보를 알 수 있는 헤더는 'User_Agent'값을 이용한다.
+			// 이를 이용해서 브라우저의 종류나 모바일인지 데스크톱인지 혹은 브라우저 프로그램의 종류를 구분할 수 있다.
+			if(userAgent.contains("Treident")) {
+				log.info("IE brower");
+				//downloadName = URLEncoder.encode(resourceName, "UTF-8").replace("//+", " ");
+				downloadName = URLEncoder.encode(resourceOriginalName, "UTF-8").replace("//+", " ");
+			}else if(userAgent.contains("Edge")) {
+				log.info("Edge browser");
+				downloadName = URLEncoder.encode(resourceOriginalName, "UTF-8");
+			}else {
+				log.info("Chrome browser");
+				downloadName = new String(resourceOriginalName.getBytes("UTF-8"), "ISO-8859-1");
+				log.info("DOWNLOAD FILENAME : " + downloadName);
+			}
+		
+			//headers.add("Content-Disposition", "attachment; filename=" + new String(resourceName.getBytes("UTF-8"),"ISO-8859-1"));
+			headers.add("Content-Disposition", "attachment; filename=" + downloadName);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity<Resource>(resource, headers, HttpStatus.OK);
 	}
 }
--- ex05/src/main/webapp/WEB-INF/views/uploadAjax.jsp
+++ ex05/src/main/webapp/WEB-INF/views/uploadAjax.jsp
@@ -7,6 +7,26 @@
 
 <script src="http://code.jquery.com/jquery-latest.min.js"></script>
 
+<style>
+.uploadResult{
+	width:100%;
+	background-color: gray;
+}
+.uploadResult ul{
+	display:flex;
+	flex-flow: row;
+	justify-content: center;
+	align-items: center;
+}
+.uploadResult ul li {
+	list-style: none;
+	padding: 10px;
+}
+.uploadResult ul li img{
+	width : 250px;
+}
+</style>
+
 <title>UPLOAD with AJAX</title>
 </head>
 <body>
@@ -14,13 +34,41 @@
 	<div class='uploadDiv'>
 		<input type="file" name="uploadFile" multiple>
 	</div>
+	
+	<div class="uploadResult">
+		<ul>
+		
+		</ul>
+	</div>
 
 	<button id="uploadBtn">UPLOAD</button>
 
 <script>
 $(document).ready(function() {
+	
+	// FILE 확장자,사이즈 사전 처리	
+	var regex = new RegExp("(.*?)\.(exe|sh|zip|alz|jar|jpg)&");
+	var maxSize = 524880; 	// 5MB
+	
+	function checkExtension(fileName, fileSize){
+		
+		if(fileSize >= maxSize){
+			alert("파일 사이즈 초과");
+			return false;
+		}
+		
+		if(regex.test(fileName)){
+			alert("해당 종류의 파일은 업로드할 수 없습니다.");
+			return false;
+		}
+		
+		return true;
+	} // end function checkExtension
+
+// FILE 업로드 이벤트처리
+	var cloneObj = $(".uploadDiv").clone();
+	
 	$("#uploadBtn").on("click", function(e){
-		alert("click");
 		var formData = new FormData();
 		
 		var inputFile = $("input[name='uploadFile']"); 
@@ -30,7 +78,10 @@ $(document).ready(function() {
 		
 		//add filedate to formdata
 		for (var i = 0; i < files.length; i++) {
-			formData.append("uploadFile", files[i]);
+			if(!checkExtension(files[i].name, files[i].size) ){
+				return false;
+			}else{
+			formData.append("uploadFile", files[i]); }
 		}
 		
 		$.ajax({
@@ -38,15 +89,54 @@ $(document).ready(function() {
 			processData : false,
 			contentType : false,
 			data : formData,
-			type : 'POST',
-			success : function(result) {
-				slert("UPLOAD");
-			}
+				type : 'POST',
+				dataType: 'json',
+				success : function(result) {
+					console.log(result);
+					
+					showUploadedFile(result);
+					
+					$(".uploadDiv").html(cloneObj.html());
+				}
 		});	// end ajax
 	
-	});	
-});
+	});	 //end function uploadBtnclick
 	
+// 파일 이름 출력	
+	var uploadResult = $(".uploadResult ul");
+	
+	function showUploadedFile(uploadResultArr){
+		var str = "";
+		
+		$(uploadResultArr).each(
+			function(i, obj){
+				if(!obj.image){
+					var fileCallPath = encodeURIComponent( obj.uploadPath + "/" + obj.uuid + "_" + obj.fileName );
+					
+					str += "<li><a href='/download?fileName=" + fileCallPath + "'>"
+							+ "<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQqBkWVMOwxWPv4jDWhaytTugDikyu4f45V4-qWOrLnP1e51XBl'>"
+							+ obj.fileName + "</li>";
+				}else{
+					//str += "<li>" + obj.fileName + "<li>";
+ 					var fileCallPath = encodeURIComponent( obj.uploadPath + "/s_" + obj.uuid + "_" + obj.fileName );					
+					
+ 					var originPath = obj.uploadPath + "\\" + obj.uuid + "_" + obj.fileName;
+ 					
+ 					originPath = originPath.replace(new RegExp(/\\/g), "/" );
+					 					
+					//str += "<li><img src='/display?fileName="+fileCallPath+"'><li>";
+					str += "<li><a href=\"javascript:showImage(\'" + originPath + "\')\"><img src='/display?fileName="
+						+ fileCallPath + "'></a><li>";
+				}
+			});
+		uploadResult.append(str);
+	} // end function showUploadedFile
+
+}); // end $(document).ready 
+
+function showImage(fileCallPath){
+	alert(fileCallPath);
+}
 </script>	
 </body>
 </html>
